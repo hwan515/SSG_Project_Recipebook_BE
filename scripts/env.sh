07c0e46ceb75f948fdc727489ee8f54568452cd4
hwan515 <stophwan515@gmail.com>
2023-11-27 17:58:58 +0900

    modified:   scripts/env.sh

--- scripts/env.sh
+++ scripts/env.sh
@@ -2,10 +2,10 @@ echo ">>> set env --------------------------"
 export FLASK_APP=app
 export REDIS_HOST=redis
 export APP_CONFIG_FILE=/home/ubuntu/recipe_BE/config/production.py
-export DB_USER=postgres
+export DB_USER=admin
 export DB_PASSWORD=password
 export DB_HOST=hwan-db.cwjlce28o2o9.ap-southeast-1.rds.amazonaws.com
-export DB_PORT=5432
+export DB_PORT=3306
 export DB_NAME=recipe
 export SECRET_KEY=b'X2t\x05\xf5\xcb\xc1\xa5`\xd6"\xfb \x0c\x14\xb5'
 export JWT_SECRET_KEY=b'\x10\xc8\xf9S\x1c&\x9fAD\x82\xf5\xa1\xdbx\xea>'
