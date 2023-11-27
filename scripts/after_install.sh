cf494c01763aaedbcf1e602845b6826c87bbcb3c
hwan515 <stophwan515@gmail.com>
2023-11-27 12:00:43 +0900

    modified:   scripts/after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -14,3 +14,6 @@ rm -rf appspec.yml requirements.txt
 
 echo ">>> change owner to ubuntu -----------"
 chown -R ubuntu /home/ubuntu/recipe_BE
+
+echo echo ">>> start server -----------"
+flask run --host=0.0.0.0
