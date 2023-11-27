6e334568f7b711ee0bcd3656dc9f777f2aa9093e
hwan515 <stophwan515@gmail.com>
2023-11-27 20:04:19 +0900

    modified:   scripts/after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -20,11 +20,11 @@ chmod +x /home/ubuntu/recipe_BE/scripts/env.sh
 cd /home/ubuntu/recipe_BE/scripts
 source ./env.sh
 
-# cd   /home/ubuntu/recipe_BE
+cd   /home/ubuntu/recipe_BE
 
-# flask db init
-# flask db migrate
-# flask db upgrade
+flask db init
+flask db migrate
+flask db upgrade
 
-# echo ">>> start server ---------------------"
-# nohup flask run --host=0.0.0.0 > log.txt 2>&1 &
+echo ">>> start server ---------------------"
+nohup flask run --host=0.0.0.0 > log.txt 2>&1 &
