baad27cf95cb233bdd1fadad14980eede47c9415
hwan515 <stophwan515@gmail.com>
2023-11-28 17:57:35 +0900

    modified:   appspec.yml 	modified:   scripts/after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -26,4 +26,6 @@ cd   /home/ubuntu/recipe_BE
 # flask db migrate
 # flask db upgrade
 
+echo ">>> start server ---------------------"
+gunicorn --bind 0.0.0.0:5000 --timeout 90 "app:create_app()" > /home/ubuntu/gunicorn.log 2>&1 &
 
