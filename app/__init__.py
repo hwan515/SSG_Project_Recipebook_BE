1ed8644b9e6aa65580ca3e4c911729557b4e47c5
hwan515 <stophwan515@gmail.com>
2023-11-28 17:45:37 +0900

    modified:   app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -19,7 +19,7 @@ naming_convention = {
     "uq": "uq_%(table_name)s_%(column_0_name)s",
     "ck": "ck_%(table_name)s_%(column_0_name)s",
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-    "pk": "pk_%(table_name)s",
+    "pk": "pk_%(table_name)s"
 }
 
 
