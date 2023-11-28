a58e114703e630934896ce993b25ef497d35cd4a
hwan515 <stophwan515@gmail.com>
2023-11-28 16:16:33 +0900

    modified:   scripts/before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 var=$(ps -ef | grep 'gunicorn' | grep -v 'grep')
-pid1=$(echo "${var}" | awk '{print $2}')
-pid2=$(echo "${var}" | awk '{print $16}')
+pid1=$(echo "${var}" | cut -d " " -f2)
+pid2=$(echo "${var}" | cut -d " " -f16
 
 if [ -n "${pid1}" ] && [ -n "${pid2}" ]; then
-    sudo kill -9 "${pid1}"
-    sudo kill -9 "${pid2}"
+    sudo kill -9 "${pid1} , ${pid2}"
     echo "${pid1} and ${pid2} are terminated."
 else
     echo "gunicorn processes are not running."
