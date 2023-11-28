0a1a8ff9718af75cb388639ea842e5adb7615f20
hwan515 <stophwan515@gmail.com>
2023-11-28 17:41:03 +0900

    modified:   scripts/before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,7 +2,7 @@
 
 var=$(ps -ef | grep gunicorn | grep -v grep | tr -s " "  | cut -d " " -f2)
 pid1=$(echo ${var} | cut -d " " -f1)
-pid2=$(echo "${pid}" | cut -d " " -f1)
+pid2=$(echo ${var} | cut -d " " -f2)
 
 if [ -n "${pid1}" ] && [ -n "${pid2}" ]; then
     sudo kill -9 ${pid1}
