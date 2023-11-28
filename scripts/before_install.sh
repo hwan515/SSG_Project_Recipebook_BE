a766884316638168c2e6085db672dec77d980b4b
hwan515 <stophwan515@gmail.com>
2023-11-28 14:50:05 +0900

    modified:   scripts/before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-var=$(ps -ef | grep 'flask run' | grep -v 'grep')
-pid=$(echo ${var} | cut -d " " -f2)
+var=$(ps -ef | grep 'gunicorn' | grep -v 'grep')
+pid1=$(echo ${var} | cut -d " " -f2)
+pid2=$(echo ${var} | cut -d " " -f16)
 if [ -n "${pid}" ]
 then 
-    sudo kill -9 ${pid}
-    echo ${pid} is terminated.
+    sudo kill -9 ${pid1}
+    sudo kill -9 ${pid2}
+    echo ${pid1} is terminated.
 else
-    echo ${pid} is not running.
+    echo ${pid1} is not running.
 fi
 
 rm -rf /home/ubuntu/recipe_BE
