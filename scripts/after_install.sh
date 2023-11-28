a5cd8ba3e976cb471d0718f84bb1dac04c4a213a
hwan515 <stophwan515@gmail.com>
2023-11-28 18:18:45 +0900

    modified:   scripts/after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -15,10 +15,10 @@ rm -rf appspec.yml requirements.txt
 echo ">>> change owner to ubuntu -----------"
 chown -R ubuntu /home/ubuntu/recipe_BE
 
-# echo ">>> set env --------------------------"
-# chmod +x /home/ubuntu/recipe_BE/scripts/env.sh
-# cd /home/ubuntu/recipe_BE/scripts
-# source ./env.sh
+echo ">>> set env --------------------------"
+chmod +x /home/ubuntu/recipe_BE/scripts/env.sh
+cd /home/ubuntu/recipe_BE/scripts
+source ./env.sh
 
 cd   /home/ubuntu/recipe_BE
 
