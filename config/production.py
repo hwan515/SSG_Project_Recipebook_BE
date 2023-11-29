ce1f966ed50d66ad20947a34f2c3cc6f153620f7
hwan515 <stophwan515@gmail.com>
2023-11-29 11:45:48 +0900

    modified:   config/production.py

--- config/production.py
+++ config/production.py
@@ -11,7 +11,7 @@ DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_HOST = os.getenv('DB_HOST')
 DB_PORT = os.getenv('DB_PORT')
 DB_NAME = os.getenv('DB_NAME')
-SQLALCHEMY_DATABASE_URI = f'mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}&collation=utf8mb4_general_ci'
+SQLALCHEMY_DATABASE_URI = f'mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
 # 시크릿 키 설정
 SECRET_KEY = os.getenv('SECRET_KEY')
